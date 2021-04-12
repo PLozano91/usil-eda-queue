c67909f6251dbeb8a2675e001d0e4acfffc1f166
Angel Mejia <66014666+ins4w@users.noreply.github.com>
2021-04-12 07:53:57 -0500

    Queue Header
    Init setup for queue

---
+++ queue.h
@@ -0,0 +1,29 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+// #include <vector>
+using namespace std;
+
+template <typename T>
+class queue
+{
+  private:
+  // members here
+  public:
+    void add(T &elem);
+    T remove();
+};
+
+template <typename T>
+void Stack<T>::add(T &elem)
+{
+
+}
+
+template <typename T>
+T Stack<T>::remove()
+{
+
+}
+
+#endif
